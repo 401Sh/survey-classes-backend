7a2c1cbac4a1f18b9ccf23d96ce936374ff3939b
401Sh <barash1a@gmail.com>
2026-05-15 15:24:34 +0000

    refactor: fix auth throttle limit incorrect constant

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,11 +12,11 @@ import { ForgotPasswordBodyDto } from "./dto/forgot-password-body.dto"
 import { ForgotPasswordConfirmBodyDto } from "./dto/forgot-password-confirm-body.dto"
 import { ResetPasswordBodyDto } from "./dto/reset-password-body.dto"
 import { Throttle } from "@nestjs/throttler"
-import { AUTH_THROTTLE_TTL } from "src/common/constants/throttle.constant"
+import { AUTH_THROTTLE_LIMIT, AUTH_THROTTLE_TTL } from "src/common/constants/throttle.constant"
 
 @Throttle({ default: {
     ttl: AUTH_THROTTLE_TTL,
-    limit: AUTH_THROTTLE_TTL,
+    limit: AUTH_THROTTLE_LIMIT,
 }})
 @Controller("auth")
 export class AuthController {
