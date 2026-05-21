0437b5578d62cf8b0ab7c6f76f4123774aa0826f
401Sh <barash1a@gmail.com>
2026-05-21 07:37:49 +0000

    refactor: fix vitest commonjs conflict

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,17 +5,17 @@ import { resolve } from "path"
 export default defineConfig({
     test: {
         globals: true,
-        root: './',
-        environment: 'node',
+        root: "./",
+        environment: "node",
     },
     resolve: {
         alias: {
-            src: resolve(__dirname, './src'),
+            src: resolve(__dirname, "./src"),
         },
     },
     plugins: [
         swc.vite({
-            module: { type: 'es6' },
+            tsconfigFile: "./tsconfig.test.json",
         }),
     ],
 })
